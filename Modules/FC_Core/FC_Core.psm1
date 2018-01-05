f60f4affd6b5614e8f58904e21630a1f0a7da409
Brandon McClure <brandon.mcclure@dhha.org>
2018-01-05 14:14:40 -0700

    New function to workaround an issue with creating a PSRepository pointing to our internal TFS package feed. It is using a HTTPS endpoint and apparently PowerShellGet does not handle that well.

--- Modules/FC_Core/FC_Core.psm1
+++ Modules/FC_Core/FC_Core.psm1
@@ -261,3 +261,48 @@ foreach ($dir in $subDirs){
 
 cd $origDirectory
 }Export-modulemember -Function Invoke-GitFetchForSubDirectories
+Function Register-PSRepositoryFix {
+<#
+    .Synopsis
+      This function is used as a workaround for an apparent bug when registering a PSRepository that uses an SSL endpoint. See https://stackoverflow.com/questions/35296482/invalid-web-uri-error-on-register-psrepository/35296483
+    .LINK
+       Source - https://stackoverflow.com/questions/35296482/invalid-web-uri-error-on-register-psrepository/35296483
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]
+        $Name,
+
+        [Parameter(Mandatory=$true)]
+        [Uri]
+        $SourceLocation,
+
+        [ValidateSet('Trusted', 'Untrusted')]
+        $InstallationPolicy = 'Trusted'
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    Try {
+        Write-Verbose 'Trying to register via ​Register-PSRepository'
+        ​Register-PSRepository -Name $Name -SourceLocation $SourceLocation -InstallationPolicy $InstallationPolicy
+        Write-Verbose 'Registered via Register-PSRepository'
+    } Catch {
+        Write-Verbose 'Register-PSRepository failed, registering via workaround'
+
+        # Adding PSRepository directly to file
+        Register-PSRepository -name $Name -SourceLocation $env:TEMP -InstallationPolicy $InstallationPolicy
+        $PSRepositoriesXmlPath = "$env:LOCALAPPDATA\Microsoft\Windows\PowerShell\PowerShellGet\PSRepositories.xml"
+        $repos = Import-Clixml -Path $PSRepositoriesXmlPath
+        $repos[$Name].SourceLocation = $SourceLocation.AbsoluteUri
+        $repos[$Name].PublishLocation = (New-Object -TypeName Uri -ArgumentList $SourceLocation, 'package/').AbsoluteUri
+        $repos[$Name].ScriptSourceLocation = ''
+        $repos[$Name].ScriptPublishLocation = ''
+        $repos | Export-Clixml -Path $PSRepositoriesXmlPath
+
+        # Reloading PSRepository list
+        Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
+        Write-Verbose 'Registered via workaround'
+    }
+}Export-Modulemember -Function Register-PSRepositoryFix 
