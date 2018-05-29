bbc6d4f4240803ed9983b229a00411ce3083e647
Brandon McClure <brandonmcclure89@gmail.com>
2018-05-28 19:11:29 -0600

    Updated module manifests using my new and improved Publish-AllFCModules script. This exports the functions better, does semantic versioning as stuff changes, and allows me to store/update the other manifest data separately from the .psd1 file itself.

--- Modules/Publish-ALLFCModules.ps1
+++ Modules/Publish-ALLFCModules.ps1
@@ -4,12 +4,38 @@ param(
     [parameter(Mandatory=$false)][string] $moduleName = $null
     ,[parameter(Mandatory=$false)][string]$moduleDescription = $null
     ,[string] $moduleAuthor = "Brandon McClure"
-    ,[switch] $forceExport
+    ,[switch] $forceConfigUpdate = $true
     )
 
 if ([string]::IsNullOrEmpty($logLevel)){$logLevel = "Info"}
 Set-LogLevel $logLevel
 $origLocation = Get-Location
+
+function Update-ManifestFromConfig{
+param($ManifestConfigPath,$ManifestPath,$moduleName)
+    Write-Host "Loading configuration data from $ManifestConfigPath"
+    $configData = Get-Content $ManifestConfigPath | ConvertFrom-Json
+    if (Test-Path $ManifestPath){
+        Write-Host "Manifest already exists"
+        if (![string]::IsNullOrEmpty($configData.Author)){
+            Update-ModuleManifest -Path $ManifestPath -Author $configData.Author
+        }
+        if (![string]::IsNullOrEmpty($configData.Description)){
+            Update-ModuleManifest -Path $ManifestPath -Description $configData.Description
+        }
+        if (![string]::IsNullOrEmpty($moduleName)){
+            Update-ModuleManifest -Path $ManifestPath -RootModule $moduleName
+        }
+        if (![string]::IsNullOrEmpty($configData.PSVersion)){
+            Update-ModuleManifest -Path $ManifestPath -PowerShellVersion $configData.PSVersion
+        }
+    }
+    else{
+        New-ModuleManifest -Path $ManifestPath -Author $configData.Author -Description $configData.Description -RootModule $moduleName -ModuleVersion "1.0" -PowerShellVersion $configData.PSVersion -RequiredModules $configData.requiredModules -NestedModules $configData.nestedModules | Out-Null
+    }
+    Test-ModuleManifest -Path $ManifestPath -ErrorAction Stop
+    Write-Log "Module manifest creation/testing complete"
+}
 try{
     $modules = Get-ChildItem -Recurse | where {$_.Extension -eq '.psm1'}
     foreach($module in $modules){
@@ -18,6 +44,7 @@ try{
         $moduleDir = Split-Path $module.FullName -Parent
         $ManifestPath = "$moduleDir\$moduleName.psd1"
         $ManifestConfigPath = "$moduleDir\moduleManifest.json"
+        $updateManifestFromConfig = 0
         
         Write-Host "Checking the $ModuleName module"
         Write-Host "At: $modulePath"
@@ -30,12 +57,19 @@ try{
                 break
             }
 
-            $configData = Get-Content $ManifestConfigPath | ConvertFrom-Json
+            $updateManifestFromConfig = 1
+        }
 
-            New-ModuleManifest -Path $ManifestPath -Author $configData.Author -Description $configData.Description -RootModule $moduleName -ModuleVersion "1.0" -PowerShellVersion $configData.PSVersion -RequiredModules $configData.requiredModules -NestedModules $configData.nestedModules | Out-Null
-            Test-ModuleManifest -Path $ManifestPath -ErrorAction Stop
-            Write-Log "Module manifest creation/testing complete"
-
+        if ($updateManifestFromConfig -eq 1 -or $forceConfigUpdate){
+            if ($forceConfigUpdate){
+                Write-Host "Forcibly updating the manifest from the config if it exists"
+            }
+            If(!(Test-Path $ManifestConfigPath)){
+                Write-Host "Manifest config does not exist, skipping"
+            }
+            else{
+                Update-ManifestFromConfig -ManifestConfigPath $ManifestConfigPath -ManifestPath $ManifestPath -moduleName $moduleName
+            }
         }
 
         Import-Module $modulePath -ErrorAction Stop
