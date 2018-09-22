5824d55d14ba0e7712642a4826d4d57adee1ec13
Brandon McClure <brandon.mcclure@dhha.org>
2018-09-22 13:43:55 -0600

    Added documentation

--- Modules/FC_Log/Public/Write-Log.ps1
+++ Modules/FC_Log/Public/Write-Log.ps1
@@ -1,25 +1,34 @@
 ﻿function Write-Log {
 <#
     .Synopsis
-       Writes a message to one of several streams. The default "info" messages use Write-Information, 
+       Writes a message to one of several streams/targetd. The default "info" messages use Write-Information and set the informationprefer
 	.Description
-        Write-Log is the central function for the logger module. It is designed to wrap the several ways to log data into one easy to use function. Using Write-Log in your script, you can then decide at run time if you want your messages to display or not, and to what stream you want them written. A common use is to Set-logLevel to Error when the scripts are running in production, and then if an error occurs reruning the scripts with Debug level, and possible write to the Windows event log to help you in troubleshooting. 
+        Write-Log is the central function for the FC_Log module. It is designed to wrap the several ways to log data into one easy to use function. Using Write-Log in your script, you can then decide at run time if you want your messages to display or not, and to what stream you want them written. A common use is to Set-logLevel to Error when the scripts are running in production, and then if an error occurs reruning the scripts with Debug level, and possible write to the Windows event log to help you in troubleshooting. 
+
+        Messages can be written to the windows event log in addition with being returned via a powershell stream based on how the logger has been configured. 
+        
+        There is also limited formatting that will be applied. The formatiing is set in the Set-LogFormattingOptions function 
         
 		NOTE: When you use -ErrorAction Stop on this function it will not write any errors in the calling scope
 	.PARAMETER Message
-		The $Message parameter is the text that will be written. Position=0
+        Default: $null
+        Position: 0
+
+		The $Message parameter is the string that will be written. 
 	.PARAMETER EventLevel
+        Position: 2
 		Specifies the entry type of the event. Position=1 
-		Valid values are "Debug","Info","Warning","Error"
+		Valid values are "Debug","Verbose","Info","Warning","Error"
 
-        The default level of Info is used if not specified. Write-Log checks against the script scoped logLevel, if the Write-Log spe
+        The default level of Info is used if not specified. Write-Log checks against the script scoped logLevel, if the EventLevel is lower than the script scoped $logLevel then the message will be written to the appropriate stream (Write-Debug,Write-Verbose,Write-Information,Write-Warning, or Write-Error)
 	.PARAMETER eventID
-		Only used when you Set-logTargetWinEvent on. The Event ID for the windows event log. Can be any number between 0 and 65535, defaults to 10. Position=3
+        Position:  3
+		Only used when you Set-logTargetWinEvent on. The Event ID for the windows event log. Can be any number between 0 and 65535, defaults to 10. 
      .EXAMPLE
-        Writes a message as a Information event level. Good for general logging to the screen. If the Verbose preference variable was set to 'Continue' this message would be written to the Verbose stream, if not it is written to host. 
-       Write-Log "Basic information message"
+        Writes a message as a Information event level. Good for general logging to the screen. The Information preference variable is set to 'Continue' for the scope of the call so that the message will be deployed regardless of the caller's preference.
 
-       
+       Write-Log "Basic information message"
+ 
 	.EXAMPLE
         Writing a error message to the Windows Event Log a message as a Error event level, with the max eventID value of 65535. This will call Write-Error to raise an error on the PowerShell host, and write the log to the Windows Event log with the event ID: 65535
 
@@ -75,7 +84,7 @@
       Write-Debug "$Message"
     }
     else {
-        $VerbosePreference = 'Continue'
+      $VerbosePreference = 'Continue'
       Write-Verbose "$FormatMessage"
     }
     if ($script:logTargetWinEvent -eq 1) {
@@ -87,7 +96,7 @@
 
   }
   #Verbose Messages
-  elseif ($messageLevel -eq 5 -and $script:LogLevel -le 5){
+  elseif ($messageLevel -eq 5 -and $script:LogLevel -le 5) {
     if ($script:logFormattingOptions['PrefixCallingFunction'] -eq 1 -and !([string]::IsNullOrEmpty($callingFunction))) {
       $FormatMessage = "$tabs$timeStamp[$callingFunction] $Message"
     }
@@ -95,8 +104,8 @@
       $FormatMessage = "$tabs$timeStamp $Message"
     }
     $VerbosePreference = 'Continue'
-      Write-Verbose "$FormatMessage"
-       if ($script:logTargetWinEvent -eq 1) {
+    Write-Verbose "$FormatMessage"
+    if ($script:logTargetWinEvent -eq 1) {
       Write-EventLog -LogName Application -Source "$script:LogSource" -EntryType "Information" -EventId $eventID -Message "$FormatMessage"
     }
   }
@@ -108,8 +117,8 @@
     else {
       $FormatMessage = "$tabs$timeStamp$Message"
     }
-      $InformationPreference = 'Continue'  
-      Write-Information $FormatMessage
+    $InformationPreference = 'Continue'
+    Write-Information $FormatMessage
     if ($script:logTargetWinEvent -eq 1) {
       Write-EventLog -LogName Application -Source "$script:LogSource" -EntryType "Information" -EventId $eventID -Message "$FormatMessage"
     }
