b15d0ab09d9181c97f17a647325a951819a1f13f
Brandon McClure <brandon.mcclure@dhha.org>
2018-09-24 13:50:59 -0600

    Export-ExcelToTxt - Allow the user to specify the file type to save as New-SQLTableStatementFromDataTable - use the new function to get the data type New functions to get a alter statement, and get the datatype either from the column metadata or from the data in the datatable itself

--- Modules/FC_Data/FC_Data.psm1
+++ Modules/FC_Data/FC_Data.psm1
@@ -3,6 +3,58 @@
 [string]$Script:JobsCompleteFlag = "$($Script:JobPrefix)Complete"
 $script:SSISLogLevels = @{ "None" = 0; "Basic" = 1; "Performance" = 2; "Verbose" = 3 }
 
+class iDataImport{
+    [boolean]$ErrorExists
+    [Exception]$ErrorException
+    [int]$ErrorLine
+    [string]$ErrorMessage
+}
+class DataImportFile:iDataImport {
+    [string]$FilePath
+    [string]$FileName
+    [string]$schemaName
+    [string]$tableName
+    [string]$FQTableName
+    [string]$sqlprojIncludes
+    [string]$sqlprojCreateScript
+    [string]$sqlCommand
+    [string]$destServerName
+    [string]$destDatabase
+    
+    [string]$fileHTMLReport
+    [DataImportFileSummary] $ImportSummary    
+
+    DataImportFile(){
+        $this.ImportSummary = New-Object DataImportFileSummary
+    }
+}
+
+class DataImportSummary{
+    [int]$numberOfRecordsToLoad
+    [int]$numberOfRecordsFailedToLoad
+
+    DataImportSummary(){
+        $this.numberOfRecordsToLoad = 0
+        $this.numberOfRecordsFailedToLoad = 0
+    }
+}
+class DataImportFileSummary : DataImportSummary{
+    [string]$ColumnsInDBNotInFile
+    [int]$NumColumnsNotInFile
+    [string]$ColumnsAddedToDB
+    [int]$NumColumnsAddedToDB
+    [boolean]$DoesTableNeedToBeCreated
+
+    DataImportFileSummary(){
+        $this.ColumnsInDBNotInFile = ""
+        $this.NumColumnsNotInFile = 0
+        $this.ColumnsAddedToDB = ""
+        $this.NumColumnsAddedToDB = 0
+        $this.DoesTableNeedToBeCreated = $false
+    }
+}
+
+
 Write-Verbose "Importing Functions"
 
 # Import everything in sub folders folder 
