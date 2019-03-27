2169229e423c2ba40cd42d0553e05e21847b11cb
Brandon McClure <Brandon.McClure@dhha.org>
2019-03-27 09:19:44 -0600

    create a property for parent relationships

--- Modules/FC_TFS/Public/New-TFSWorkItemBody.ps1
+++ Modules/FC_TFS/Public/New-TFSWorkItemBody.ps1
@@ -23,8 +23,30 @@ foreach ($field in $itemDefinition.keys){
         Write-Log "Could not find a field with the name: $($field)" Error -ErrorAction Stop
     }
 
+    
+
     $fieldReference = $script:fieldDefinition | where {$field -in $_.FieldAlias}| select -ExpandProperty FieldPath
 
+    if($field -eq "relationship_parent"){
+     $workItemBody += @"
+$(AmmendCommasOnWorkItemBody $workItemBody)
+{
+            "op": "add",
+            "path": "/relations/-",
+            "value":
+            {
+                "rel": "System.LinkTypes.Hierarchy-Reverse",
+                "url": "$($itemDefinition[$field])",
+                "attributes":
+                {
+                    "isLocked": false,
+                "comment": "Automagical relationship"
+                }
+            }
+        }
+"@
+        continue;
+    }
     $workItemBody += @"
 $(AmmendCommasOnWorkItemBody $workItemBody)
     {
