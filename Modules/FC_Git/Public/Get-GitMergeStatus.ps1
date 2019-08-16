a2dbe5404650e0f7ac926fe6ced57aa9a8d3c42e
Brandon McClure <Brandon.McClure@dhha.org>
2019-08-16 12:25:34 -0600

    New-GitBranch Don't try to sync. Let me handle that on my own.

---
+++ Modules/FC_Git/Public/Get-GitMergeStatus.ps1
@@ -0,0 +1,17 @@
+﻿function Get-GitMergeStatus {
+
+    $result = Start-MyProcess "git" "merge HEAD" -sleepTimer 0
+
+
+    if ([string]::IsNullOrEmpty($result.stderr) ){
+      Write-Output "Ready to Merge"
+    }
+    else {
+        if ($result.stderr -like '*unresolved conflict*'){
+            Write-Output 'Merge Conflict'
+        }
+        else{
+            Write-Output "Unknown status"
+        }
+  }
+} Export-ModuleMember -Function Get-GitMergeStatus
