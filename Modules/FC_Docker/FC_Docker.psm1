58f3f8a8a4b1844600f48faeafc52eb1aa6cb979
Brandon McClure <brandonmcclure89@gmail.com>
2021-02-28 08:42:45 -0700

    Create a class with a fully qualified image name

--- Modules/FC_Docker/FC_Docker.psm1
+++ Modules/FC_Docker/FC_Docker.psm1
@@ -1,3 +1,35 @@
+#Parts of a docker fully qualified image name
+class DockerFQName{
+    [string]$Registry
+	[string]$Repository
+	[string]$Image
+	[string]$Tag
+	[String]$FQN
+
+	DockerFQName($registry,$repository,$image,$tag){
+        if([string]::IsNullOrEmpty($registry) -or $registry.EndsWith('/')){
+			$this.Registry = ($registry ?? "").tolower()
+		}else{
+			$this.Registry = ($registry ?? "").tolower()+'/'
+		}
+		if([string]::IsNullOrEmpty($repository) -or $repository.EndsWith('/')){
+			$this.Repository = ($repository ?? "").tolower()
+		}else{
+			$this.Repository = ($repository ?? "").tolower()+'/'
+		}
+		$this.Image = ($image ?? "").tolower()
+
+		if([string]::IsNullOrEmpty($tag)){
+			$this.Tag = "latest"
+		}
+		else{
+
+			$this.Tag = ($tag.replace(":","")).tolower()
+		}
+		$this.FQN = "$($this.Registry)$($this.Repository)$($this.Image):$($this.Tag)"
+    }
+}
+
 Write-Verbose "Importing Functions"
 
 # Import everything in sub folders folder 
