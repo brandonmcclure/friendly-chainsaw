abd750183f933d7ae72344c8dd8dff70ee388586
Brandon McClure <brandonmcclure89@gmail.com>
2021-11-13 16:47:09 -0700

    new function for writing a prometheus metric file. Update the batch ending function Invoke-PrometheusBatchEnding() so that it uses the Invoke-PrometheusMetricFile()

--- Modules/FC_Log/Public/Invoke-PrometheusBatchEnding.ps1
+++ Modules/FC_Log/Public/Invoke-PrometheusBatchEnding.ps1
@@ -1,18 +1,22 @@
 function Invoke-PrometheusBatchEnding{
-    param($SLO_InstanceShouldRunEveryXSeconds)
-
+    param(
+        $textFileDir = "C:\mcd\promMetrics",
+    $SLO_InstanceShouldRunEveryXSeconds,
+    $SupportTeam = "Unsupported"
+    )
+    $JobType = 'batch'
     if([string]::IsNullOrEmpty($SLO_InstanceShouldRunEveryXSeconds)){
         Write-Log "Cannot send prometheus batch ending. Please pass the target SLO for how many seconds we expect this batch job to run via -SLO_InstanceShouldRunEveryXSeconds" Error -ErrorAction Stop
     }
     $unixEpochStart = new-object DateTime 1970,1,1,0,0,0,([DateTimeKind]::Utc)
     $unixEpochTimer = [int]([DateTime]::UtcNow - (new-object DateTime 1970, 1, 1, 0, 0, 0,([DateTimeKind]::Utc))).TotalSeconds
 
-    # Our SLO is to run an instance of this script every hour, 24/7
-    $sloGauge = New-PrometheusMetricGauge -metricName "instance_last_complete_slo_target_seconds" -MetricDescription "The target SLO threshold for how frequently we are planning on running this batch (in seconds)."
-    $sloGauge.set($SLO_InstanceShouldRunEveryXSeconds);
-    $sloGauge.Publish();
+    $metrics = @(
+        @{Name="mcd_data_instance_last_complete_epoch_seconds_diff"; Description="The last time this job finished";type="gauge"; value="$($unixEpochTimer.ToString())"}
+        ,@{Name="mcd_instance_last_complete_slo_target_seconds"; Description="The target SLO threshold for how frequently we are planning on running this batch (in seconds).";type="gauge"; value="$($SLO_InstanceShouldRunEveryXSeconds.ToString())"}
+    )
 
-    $timeGauge = New-PrometheusMetricGauge -metricName "instance_last_complete_epoch_seconds_diff" -MetricDescription "The last time this job finished"
-    $timeGauge.set($unixEpochTimer);
-    $timeGauge.Publish();
+    $labels = @("SupportTeam=`"$SupportTeam`"")
+    Invoke-PrometheusMetricFile -metrics $metrics -textFileDir $textFileDir -labels $labels
+    
 }
