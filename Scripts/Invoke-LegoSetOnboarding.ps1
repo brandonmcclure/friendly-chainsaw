de7edc33ebab896caea7329e8c2f6ea208e9f1bd
Brandon McClure <brandonmcclure89@gmail.com>
2022-02-23 19:55:34 -0700

    add comma

--- Scripts/Invoke-LegoSetOnboarding.ps1
+++ Scripts/Invoke-LegoSetOnboarding.ps1
@@ -5,7 +5,7 @@
 		This script will open up N number of browser windows for you to manually download the pdf files from lego's website. 
 
 		It will also output a csv file that can be imported into rebrickable.com
-		
+
       Check out the related blog post: https://brandonmcclure.gitlab.io/orgmode/2022.02.23-lego-manual-downloads/
     .LINK
        https://brandonmcclure.gitlab.io/orgmode/2022.02.23-lego-manual-downloads/
@@ -13,7 +13,7 @@
 
 	param(
 # Enter the path to the web broser you want to use to download the files. I used Firefox, ymmv with other browsers
-$browserPath = "C:\Program Files\Mozilla Firefox\firefox.exe"
+$browserPath = "C:\Program Files\Mozilla Firefox\firefox.exe",
 
 # A list of the set numbers, the example below is for this set https://www.lego.com/en-us/service/buildinginstructions/75233
 $sets = @(
