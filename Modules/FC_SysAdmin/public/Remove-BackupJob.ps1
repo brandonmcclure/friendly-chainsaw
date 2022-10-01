64d4ac7756264198df32bfcf8ac9f53af5a96dcc
Brandon McClure <brandonmcclure89@gmail.com>
2022-10-01 09:12:50 -0600

    add help text supportsshouldprocesss add begin/process blocks

--- Modules/FC_SysAdmin/public/Remove-BackupJob.ps1
+++ Modules/FC_SysAdmin/public/Remove-BackupJob.ps1
@@ -1,10 +1,18 @@
 function Remove-BackupJob{
+<#
+.Synopsis
+	Removes a backup job from the database
+#>
+	[CmdletBinding(SupportsShouldProcess = $true)]
 	param([Parameter(ValueFromPipeline)]$job)
 
-	if([string]::IsNullOrEmpty($job)){Write-Log "You must pass a job parameter"  Error -ErrorAction Stop}
+	begin{
+		if([string]::IsNullOrEmpty($job)){Write-Log "You must pass a job parameter"  Error -ErrorAction Stop}
+	}
 
-	$Script:BackupJobs = $($Script:BackupJobs | where {$_.SourcePath -ne $job.SourcePath -and $_.DestinationPath -ne $job.DestinationPath})
-	$Script:BackupJobs | ConvertTo-Json -Depth 5 | Set-Content $Script:BackupJobPath
-	
+	process{
+		$Script:BackupJobs = $($Script:BackupJobs | where-object {$_.SourcePath -ne $job.SourcePath -and $_.DestinationPath -ne $job.DestinationPath})
+		$Script:BackupJobs | ConvertTo-Json -Depth 5 | Set-Content $Script:BackupJobPath
+	}
 
 }Export-ModuleMember -Function Remove-BackupJob
